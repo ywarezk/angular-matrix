e8ff6f84a6055067bc6ffe5e504200d4e7263d1c
ywarezk <ywarezk@gmail.com>
2020-06-16 19:47:15 +0300

    added the solution to the memory leaks

--- 01-rxjs/rxjs-ex/projects/autocomplete/src/app/app.component.ts
+++ 01-rxjs/rxjs-ex/projects/autocomplete/src/app/app.component.ts
@@ -1,21 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, mergeMap, tap } from 'rxjs/operators';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   template: `
   <div>
     <form>
-      <input name="search" type="search" placeholder="Search..." />
+      <input name="search" type="search" placeholder="Search..." (input)="search($event.target.value)" />
     </form>
     <ul>
-      <li *ngFor="let item of items">
-        {{ item }}
+      <li *ngFor="let item of items$ | async">
+        {{ item.title }}
       </li>
     </ul>
   </div>
   `,
 })
-export class AppComponent {
-  public items: any[] = [];
+export class AppComponent implements OnInit {
+  public items$: Observable<any>;
+  search$: Subject<string> = new Subject();
+  
+  constructor(private _todoService: TodoService) {}
+
+  search(searchStr: string) {
+    this.search$.next(searchStr);
+  }
+
+  // stack -> one main thread -> js engine V8
+  // only the main thread can do the dom manipulation
+
+  /**
+   * throttling
+   */
+  ngOnInit() {
+    this.items$ = this.search$.pipe(
+      debounceTime(1000),
+      tap((searchStr) => console.log(searchStr)),
+      distinctUntilChanged(),
+      tap((searchStr) => console.log(searchStr)),
+      mergeMap((searchStr: string) => this._todoService.search(searchStr))
+    );
+  }
 }
 
--- 01-rxjs/rxjs-ex/projects/memory-leaks/src/app/home/home.component.ts
+++ 01-rxjs/rxjs-ex/projects/memory-leaks/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterEventsService } from '../router-events.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,20 +9,18 @@ import { RouterEventsService } from '../router-events.service';
       List of router events
     </h1>
     <ul>
-      <li *ngFor="let item of events">
+      <li *ngFor="let item of events$ | async">
         {{ item }}
       </li>
     </ul>
   `
 })
 export class HomeComponent implements OnInit {
-  events: string[] = [];
+  events$: BehaviorSubject<string[]>;
 
   constructor(private _routerEvents: RouterEventsService) { }
 
   ngOnInit() {
-    this._routerEvents.routerEvents.subscribe((e) => {
-      this.events = e;
-    }); 
+    this.events$ = this._routerEvents.routerEvents
   }
 }
